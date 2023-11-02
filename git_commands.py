197e83a00a15d14c320e2d586db92950d8b4d3eb
Mahaboob-Basha-Akula <bashaa8317@gmail.com>
2023-11-02 12:51:51 +0530

    updated git and github commands with py extention

---
+++ git_commands.py
@@ -0,0 +1,42 @@
+git init
+---Initializes an empty repository
+
+git status--It gives the information about the which  branch we r in and how many commints we r done
+
+                  OR
+                  it gives the information of current status of th git repo
+
+BRANCH:
+
+git checkout -b <branchName> ======= Switching to new branch ,
+
+git checkout <branchName> --------if we want to switch existing branch                           
+
+git add <nameOfFile>--------used to staged the untarcked files
+
+git branch --------------- Gives the info/list about all the repos we have 
+
+git commit -m "message what we have write" -------  used to track the staged files after being added by git add 
+
+git add .   ---------- Used To add all u r files at once we use 
+   
+git restore --staged <fileName> -------- used to unstage one or more files from u r files 
+
+origin means github [where the repo created]
+  
+             git staged the files after doing git add command
+
+            git tracked the files after doing git commit command and we will restore files only after commit
+
+  git log -------command is used to check the history
+
+  git revert <id of commit> -------- After deleting files we can revert it back by using  this command
+
+
+
+                                                       GITHUB
+
+
+  Repository: this is the folder where .git file reside  
+
+  orogin --- where the repo havs created [GITHUB]
